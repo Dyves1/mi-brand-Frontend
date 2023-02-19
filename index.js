dc9ca3320dd31a96645c4b4de698b36648125a1e
dyves1 <ishimwedidieryves01@gmail.com>
2023-02-20 01:28:06 +0200

    adding the local storage to blogs and works

--- index.js
+++ index.js
@@ -35,3 +35,62 @@ function validation() {
 send.onclick= validation
 
 
+// ---the blog creation
+let createTasks = () => {
+    tasks.innerHTML = "";
+    data.map((x, y) => {
+      return (tasks.innerHTML += `
+      
+      <div id=${y}>
+           <img src="${x.uploaded_image}" class="img-blog"> <br><br>
+            <span class="fw-bold">${x.text}</span><br><br>
+            <span class="set-date">${x.date}</span><br><br>
+            <p>${x.description}</p> <br><br>
+            <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-regular fa-heart" id ="like"></i>
+           
+            <i onClick ="deleteTask(this);createTasks()" class="fa-solid fa-comment" id="comment"></i>
+          </span> 
+
+      </div>
+      `);
+    });
+  
+    // resetForm();
+  };
+  (() => {
+    data = JSON.parse(localStorage.getItem("data")) || [];
+    console.log(data);
+    createTasks();
+  })();
+
+
+//   ---the work 
+// var tasks_work=document.getElementsByClassName("tasks-work")
+let createWork = () => {
+    tasks_work.innerHTML = "";
+    data_work.map((x, y) => {
+      return (tasks_work.innerHTML += `
+      
+      <div id=${y}>
+           <img src="${x.uploaded_image}" class="img-blog"><br><br> 
+            <span class="fw-bold">${x.text}</span><br><br>
+            <span class="set-date">${x.date}</span><br><br>
+            <p>${x.description}</p> <br>
+            <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-regular fa-heart" id ="like"></i>
+          
+            <i onClick ="deleteTask(this);createTasks()" class="fa-solid fa-comment" id="comment"></i>
+          </span> 
+
+      </div>
+      `);
+    });
+  
+    // resetForm();
+  };
+  (() => {
+    data_work = JSON.parse(localStorage.getItem("data_work")) || [];
+    console.log(data_work);
+    createWork();
+  })();
---
+++ nanone.js
@@ -0,0 +1,110 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let file=document.getElementById("myfile")
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
+const image_input = document.getElementById("image_input");
+var uploaded_image="";
+
+image_input.addEventListener("change",function(){
+  const reader =new FileReader();
+  reader.addEventListener("load",()=>{
+    uploaded_image=reader.result;
+    document.getElementById ("display_image").style.backgroundImage= `url("${uploaded_image}")`;
+  })
+
+  reader.readAsDataURL(this.files[0])
+})
+console.log(uploaded_image)
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
+});
+
+let formValidation = () => {
+  if (textInput.value === "") {
+    console.log("failure");
+    msg.innerHTML = "Task cannot be blank";
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+  }
+  acceptData();
+  add.setAttribute("data-bs-dismiss", "modal");
+  add.click();
+
+  (() => {
+    add.setAttribute("data-bs-dismiss", "");
+  })();
+};
+
+let data = [];
+
+let acceptData = () => {
+  data.push({
+    uploaded_image,
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
+
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log(data);
+  createTasks();
+};
+let createTasks = () => {
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+    
+    <div id=${y}>
+         <img src="${x.uploaded_image}" class="img-blog"><br>
+          <span class="fw-bold">${x.text}</span><br>
+          <span class="set-date">${x.date}</span><br>
+          <p>${x.description}</p><br>  
+          <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit" id ="edit"></i>
+            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt" id="del"></i>
+          </span>
+    </div>
+    `);
+  });
+
+  resetForm();
+};
+let resetForm = () => {
+  uploaded_image ="";
+  textInput.value = "";
+  dateInput.value = "";
+  textarea.value = "";
+};
+
+let deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+
+  data.splice(e.parentElement.parentElement.id, 1);
+
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log(data);
+};
+let editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+  uploaded_image = selectedTask.children[0].innerHTML;
+  textInput.value = selectedTask.children[1].innerHTML;
+  dateInput.value = selectedTask.children[2].innerHTML;
+  textarea.value = selectedTask.children[3].innerHTML;
+
+  deleteTask(e);
+};
+(() => {
+  data = JSON.parse(localStorage.getItem("data")) || [];
+  console.log(data);
+  createTasks();
+})();
