f55dfc5f6766fc87467a5eb88a9ed51b2579224e
dyves1 <ishimwedidieryves01@gmail.com>
2023-02-20 19:02:30 +0200

    saving the dashboard

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-icon').onclick=() =>{
     navbar.classList.toggle('active');
 }
-// function validation() {
    const nameEl= document.getElementById('name');
    const emailEl= document.getElementById('email');
    const messageEl= document.getElementById('message');
@@ -11,62 +10,8 @@ document.querySelector('#menu-icon').onclick=() =>{
    const send=document.getElementById('send')
    const form=document.getElementById('myform');
 
-//    let  text;
-   
-//    error_message.style.padding='10px'
-//    if(nameEl.length<5){
-//     text ='Please Enter valid Name';
-//     error_message.innerHTML= text;
-//     return false
-//    }
-//    else if(emailEl==''){
-//     text ='Please Enter your email';
-//     error_message.innerHTML= text;
-//     return false
-//    }
-//    else if(messageEl.length<3){
-//     text ='Please Enter the message';
-//     error_message.innerHTML= text;
-//     return false
-//    }
-//    else{
-//    alert('Message submitted successfully!')
-//     }
-
-//     // acceptMessage();
-   
-// }
-// send.onclick=acceptMessage
-//  function acceptMessage()
-
-// let message=[];
-// let acceptMessage= ()=> {
-//   message.push({
-//     newName:nameEl,
-//     newEmail:emailEl,
-//     newMessage:message
-//   })
-//   localStorage.setItem("message",JSON.stringify(message));
-//   console.log(message)
-// }
-// send.onclick= validation
-// form.addEventListener("submit",(e)=>{
-//   e.preventDefault();
-// console.log("button clicked");
-// formValidation();
-// })
-// let formValidation=()=>{
-//   if (nameEl === "") {
-//     error_message.innerHTML = "Post cannot be blank";
-//     console.log("failure");
-//   } else {
-//     console.log("successs");
-//     error_message.innerHTML = ""; 
-//   }
-// }
 let messages=[];
 function acceptMessage() {
-  // send=document.getElementById('send');
 console.log("my message")
 alert("Message sent successfully")
    messages.push({
@@ -78,11 +23,7 @@ alert("Message sent successfully")
   console.log(messages)
 }
 send.onclick = acceptMessage
-// (() => {
-//   messages = JSON.parse(localStorage.getItem("messages")) || [];
-//   console.log(messages);
-//   acceptMessage();
-// })();
+
 
 
 
